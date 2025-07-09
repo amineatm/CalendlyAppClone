efd1d62baba43fc9149f185876f16457a36b2fe2
Amine <84468278+amineatm@users.noreply.github.com>
2025-07-08 20:26:55 -0500

    initial commit

---
+++ calendly_clone_backend/src/main/java/com/calendly/calendly_clone_backend/CalendlyCloneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.calendly.calendly_clone_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendlyCloneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendlyCloneBackendApplication.class, args);
+    }
+
+}
